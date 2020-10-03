30d752e8a1c7906ebce925b032da429ad849da71
georgejs97 <george.miller97@hotmail.co.uk>
2020-10-03 16:15:00 +0100

    Adding json serialisation/deserialisation

--- Models/Folder.cs
+++ Models/Folder.cs
@@ -7,34 +7,10 @@ namespace DiaryNotes20.Models
     public Folder()
     {
       Items = new List<IPaper>();
-      RenderItems();
+      Note placeholder = new Note{ Title = "Untitled note", Body = "Undefined"};
+      Items.Add(placeholder);
     }
 
     public List<IPaper> Items { get; set; }
-
-    void RenderItems()
-    {
-      Note note1 = new Note 
-      { 
-        Title = "Todo", 
-        Body = "Go to gym, draft application for grad schemes, meet with Manos",
-        Colour = "#b00"
-      };
-      Book book = new Book { Title = "General" };
-      Note note2 = new Note 
-      {
-        Title = "Communication",
-        Body = "Ring mother, ask for appointment with consultant"
-      };
-      Note note3 = new Note
-      {
-        Title = "Fire",
-        Body = "Get haircut, shave again to be sure, REMEMBER AFTERSHAVE"
-      };
-      Items.Add(note1);
-      book.Pages.Add(note2);
-      book.Pages.Add(note3);
-      Items.Add(book);
-    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
@@ -31,13 +34,42 @@ namespace DiaryNotes20
     public static int CurrentNoteItemId { get; set; } = -1;
     public static int CurrentBookItemId { get; set; } = -1;
     public static bool CurrentBaseTypeIsNote { get; set; } = true;
-    private static bool IsInitialLoad { get; set; } = true;
+
+    public static void LoadNotes()
+    {
+      string GetData = null;
+      try
+      {
+        GetData = File.ReadAllText(@"C:\Users\Public\DiaryNotesData.json");
+      } catch (FileNotFoundException e) {
+        Console.WriteLine(e.Message, "Hence a default data file was created.");
+        GetData = @"{""Items"":[{""Title"":""Untitled book"",
+        ""Body"":"""",""Colour"":""#654321"",""Id"":420846611,""IsFav"":false,
+        ""Pages"":[{""Title"":""Untitled note"",""Body"":"""",""Colour"":
+        ""#444"",""Id"":134464447,""IsFav"":false,""Pages"":null}]},{""Title"":
+        ""Untitled note"",""Body"":"""",""Colour"":""#444"",""Id"":822834995,
+        ""IsFav"":false,""Pages"":null}]}";
+        TextWriter textWriter = new StreamWriter(
+          @"C:\Users\Public\DiaryNotesData.json"
+        );
+        textWriter.WriteLine(GetData);
+        textWriter.Close();
+      } finally {
+        Folder = JsonSerializer.Deserialize<Folder>(GetData);
+      }
+    }
+
+    public static void SaveNotes()
+    {
+      string PostData = JsonSerializer.Serialize(Folder);
+      File.WriteAllText(@"C:\Users\Public\DiaryNotesData.json", PostData);
+    }
+
     public static List<IPaper> AccessItems(int rootID=-1)
     {
-      if (IsInitialLoad && rootID==-1)
+      if (Folder == null && rootID== -1)
       {
         Folder = new Folder();
-        IsInitialLoad = false;
         return Folder.Items;
       } else if (rootID == -1) {
         return Folder.Items;
